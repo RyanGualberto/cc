594f82b7546d207704fed9644da2df161fa81ab2
ryangualberto <ryanoliveiragualberto@gmail.com>
2024-11-23 02:11:27 -0300

    changing port

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import app from "./src/app";
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080");
 });
