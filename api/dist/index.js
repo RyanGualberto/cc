2ca4e67bc8ecccf879e4b51ed7dbc1dbd8fb4417
ryangualberto <ryanoliveiragualberto@gmail.com>
2024-11-23 02:12:50 -0300

    changing port

--- api/dist/index.js
+++ api/dist/index.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
-app_1.default.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app_1.default.listen(8080, () => {
+    console.log("Server is running on http://localhost:8080");
 });
