13012f2c6bc380295541490fdc85b1c125ed6d71
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-04-05 21:13:13 -0300

    feat: configurar Vercel com arquivo vercel.json e ajustar URL base do cliente API

--- web/src/config/api-client.ts
+++ web/src/config/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-type": "application/json",
   },
